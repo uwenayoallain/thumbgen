1b4a34de8829be981a1f0bd1e7eeafbae304a1fa
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-03 20:35:30 +0200

    updated api

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 const { signup, login } = require("../controllers/user.controller");
-const { auth } = require("../middlewares/auth.middleware");
-// user
 router.post("/signup", (req, res) => signup(req, res));
 router.post("/login", (req, res) => login(req, res));
 module.exports = router;
