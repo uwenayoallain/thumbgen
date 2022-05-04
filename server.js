f1690db477d8b815e316a8dac2f8874188323b99
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-04 12:42:24 +0200

    documentation

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const userRoutes = require("./routes/user.route");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const port = process.env.PORT || 3000;
 require("dotenv").config({ path: ".env" });
 require("./utils/dbConnect")();
 const app = express();
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "ThumbGen API",
+      description: "ThumbGen API Information",
+      contact: {
+        name: "UWENAYO Alain Pacifique",
+        email: "uwenayoallain@gmail.com",
+        social: {
+          github: "http://github.com/uwenayoallain",
+          linkedin: "http://linkedin.com/in/uwenayoallain",
+          twitter: "http://twitter.com/uwenayoallain",
+        },
+      },
+      servers: [process.env.HOST + ":" + process.env.PORT],
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(cors());
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json({ extended: true }));
 app.use("/user", userRoutes);
 app.listen(port, () => {
